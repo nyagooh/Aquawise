42e2e2f6bf00fa25d6258463778feac77352ac97
Tomlee Abila <tomleeabila@gmail.com>
2026-05-20 21:37:55 +0300

    backend: register debug_toolbar URLs to fix djdt namespace KeyError in dev

--- GIS/backend/aquawise_gis/urls.py
+++ GIS/backend/aquawise_gis/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -14,3 +15,10 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    except ImportError:
+        pass
