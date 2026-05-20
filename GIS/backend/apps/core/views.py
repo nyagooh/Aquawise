3956bba57c46ffd24287a6c6140369eef38b1ee1
Tomlee Abila <tomleeabila@gmail.com>
2026-05-20 20:18:36 +0300

    GIS core: add GET /api/v1/auth/me/ endpoint returning user + organisation

--- GIS/backend/apps/core/views.py
+++ GIS/backend/apps/core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class MeView(APIView):
+    def get(self, request):
+        user = request.user
+        org = user.organisation
+        return Response({
+            "id": str(user.id),
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "role": user.role,
+            "organisation": {
+                "id": str(org.id),
+                "name": org.name,
+                "slug": org.slug,
+            } if org else None,
+        })
